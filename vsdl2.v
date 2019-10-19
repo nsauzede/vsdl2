c6f5f72917539f7c6a87b64e1e584670e8d03c52
Nicolas Sauzede <nsauzede@laposte.net>
2019-10-19 12:20:48 +0200

    Backport vsdl2 enhancements from sdl2test

--- vsdl2.v
+++ vsdl2.v
@@ -4,14 +4,20 @@
 
 module vsdl2
 
-// apparently, following line also works on non-linux ? o_O
 #flag linux `sdl2-config --cflags --libs`  -lSDL2_ttf -lSDL2_mixer
+//#flag windows `sdl2-config --cflags`
+//#flag windows `sdl2-config --libs`  -lSDL2_ttf -lSDL2_mixer
+//#flag `sdl2-config --cflags --libs`  -lSDL2_ttf -lSDL2_mixer
+
+// following kludge until `sdl2-config ...` is supported also on windows
+#flag windows -I/msys64/mingw64/include/SDL2
+#flag windows -Dmain=SDL_main
+#flag windows -L/mingw64/lib -lmingw32 -lSDL2main -lSDL2
+
 #include <SDL.h>
 #include <SDL_ttf.h>
 #include <SDL_mixer.h>
 
-fn C.SDL_CreateRGBSurface(flags u32, width int, height int, depth int, Rmask u32, Gmask u32, Bmask u32, Amask u32) &SdlSurface
-fn C.SDL_PollEvent(&SdlEvent) int
 
 struct C.SDL_Color{
 pub:
@@ -30,6 +36,25 @@ pub:
         h int
 }
 type SdlRect C.SDL_Rect
+
+struct C.SDL_Surface {
+pub:
+        flags u32
+        format voidptr
+        w int
+        h int
+        pitch int
+        pixels voidptr
+        userdata voidptr
+        locked int
+        lock_data voidptr
+        clip_rect SdlRect
+        map voidptr
+        refcount int
+}
+type SdlSurface C.SDL_Surface
+
+/////////////////////////////////////////////////////////
 
 struct SdlQuitEvent {
         _type u32
@@ -69,6 +94,7 @@ pub:
         hat byte
         value byte
 }
+
 union SdlEventU {
 pub:
         _type u32
@@ -76,25 +102,10 @@ pub:
         key SdlKeyboardEvent
         jbutton SdlJoyButtonEvent
         jhat SdlJoyHatEvent
+        _pad56 [56]byte
 }
 type SdlEvent SdlEventU
 
-struct C.SDL_Surface {
-pub:
-        flags u32
-        format voidptr
-        w int
-        h int
-        pitch int
-        pixels voidptr
-        userdata voidptr
-        locked int
-        lock_data voidptr
-        clip_rect SdlRect
-        map voidptr
-        refcount int
-}
-type SdlSurface C.SDL_Surface
 
 struct C.SDL_AudioSpec {
 pub:
@@ -110,7 +121,20 @@ mut:
 }
 type SdlAudioSpec C.SDL_AudioSpec
 
+//////////////////////////////////////////////////////////
+
+fn C.SDL_MapRGB(fmt voidptr, r byte, g byte, b byte) u32
+fn C.SDL_CreateRGBSurface(flags u32, width int, height int, depth int, Rmask u32, Gmask u32, Bmask u32, Amask u32) &SdlSurface
+fn C.SDL_PollEvent(&SdlEvent) int
+
+//////////////////////////////////////////////////////////
+
 pub fn fill_rect(screen &SdlSurface, rect &SdlRect, _col &SdlColor) {
 	col := C.SDL_MapRGB(screen.format, _col.r, _col.g, _col.b)
 	C.SDL_FillRect(screen, rect, col)
 }
+
+const (
+  version = '0.2' // hack to avoid unused module warning in the main program
+)
+
